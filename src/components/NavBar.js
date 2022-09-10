58cdf85586e0673de8c19907b3900de3ebf8fb54
mayur bhoi <mayurbhoi200@gmail.com>
2022-09-10 04:22:17 +0000

    update component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Routes, NavLink } from 'react-router-dom';
+
 
 
 export const NavBar = () =>{
     return(
+<>
+<ul>
+    <li><NavLink className = "index-link" to="/" >index</NavLink></li>
+    <li><NavLink  className = "home-link" to="/home" >home</NavLink></li>
+  
+</ul>
 
+
+    </>
     )
 }

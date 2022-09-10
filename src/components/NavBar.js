22c01d66ecaa473a73519ccc7b32780b45aa151e
mayur bhoi <mayurbhoi200@gmail.com>
2022-09-10 15:26:20 +0530

    add nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Route, Routes, NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-
-
-export const NavBar = () =>{
-    return(
-<>
-<ul>
-    <li><NavLink className = "index-link" to="/" >index</NavLink></li>
-    <li><NavLink  className = "home-link" to="/home" >home</NavLink></li>
-</ul>
-
-
-    </>
-    )
-}
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink className="index-link" to="/">
+            Index Page
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="home-link" to="/home">
+            Home Page
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
